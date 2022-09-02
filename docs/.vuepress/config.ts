82a6876eff3c34ae78f1366bd5b528a399f113d2
Haha <zhao.studio@foxmail.com>
2022-09-02 20:47:43 +0800

    modify website title

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -13,9 +13,9 @@ export default defineUserConfig({
   //网站语言，默认为中文
   lang: "zh-CN",
   //网站标题
-  title: "KnowledgeSpace-知识分享空间",
+  title: "学习与思考",
   //网站描述
-  description: "开源工具、效率方法、心理学探索的自我提升笔记，记录并输出一切能让自己提升的知识。",
+  description: "记录学习笔记、思考过程，分享学习心得与方法！",
 
   //网站路径，默认为 /，如果非主域名，需改为
   base: "/",
@@ -29,10 +29,10 @@ export default defineUserConfig({
 
   plugins: [
     //algolia 全文搜索：没设置爬虫的话，需删除 docsearchPlugin 区块以使用节点搜索
-    docsearchPlugin({
-      indexName: 'newzone',
-      appId: 'M4EXXEZIEG',
-      apiKey: 'fd8891a9c4cc21e0ef4f11bf44f7a11e',
+    //docsearchPlugin({
+    //  indexName: 'newzone',
+    //  appId: 'M4EXXEZIEG',
+    //  apiKey: 'fd8891a9c4cc21e0ef4f11bf44f7a11e',
     }),
     //本地搜索：默认情况下，该插件会将页面标题和小标题作为搜索索引。
     searchPlugin({
