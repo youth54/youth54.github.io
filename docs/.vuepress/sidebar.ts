eb8ffae37d0c2eea46bb5b3b1e2c562a2b9ad0c4
Haha <zhao.studio@foxmail.com>
2023-01-24 11:26:11 +0800

    config sidebar sublevel

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -23,12 +23,14 @@ export default sidebar([
       {
         text: "快速开始",
         icon: "",
+        prefix: "/数据分析/快速开始/",
         collapsable: true,
         children: "structure",
       },
       {
         text: "API使用详解",
         icon: "",
+        prefix: "/数据分析/API使用详解/",
         collapsable: true,
         children: "structure",
       },
